80e7c3298694a7ff204464165ffbfd80da12dd0f
grostts <grostts@proton.me>
2024-01-26 21:08:41 +0300

    The third commit. Make some changes in the project.

--- Data mining/data_mining_main.py
+++ Data mining/data_mining_main.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
-import json
 import time
 import asyncio
 import aiohttp
+import csv
 
 
 start_time = time.time()
@@ -22,6 +22,7 @@ async def get_current_vacancy_data(session, vacancy_url, vacancy_title):
         soup = BeautifulSoup(response_text, 'lxml')
 
         """Get info for every vacancy"""
+
         """Get salary"""
         try:
             vacancy_salary = soup.find(class_="vacancy-title").find(class_='bloko-header-'
@@ -29,14 +30,28 @@ async def get_current_vacancy_data(session, vacancy_url, vacancy_title):
         except Exception:
             vacancy_salary = "No salary info"
 
+        """Get company name"""
+        try:
+            vacancy_company_name = soup.find("span", class_="vacancy-company-name").text
+        except Exception:
+            vacancy_company_name = "No company name info"
+
         """Get experience"""
         try:
             vacancy_experience = soup.find(class_="vacancy-description-list-item").text
         except Exception:
             vacancy_experience = "No experience info"
 
-        print(vacancy_salary, vacancy_title, vacancy_url, vacancy_experience, sep='\n')
-        print()
+        job_vacancy_data_list.append(
+            {
+                "salary": vacancy_salary,
+                "title": vacancy_title,
+                "url": vacancy_url,
+                "company_name": vacancy_company_name,
+                "experience": vacancy_experience
+            }
+        )
+
 
 
 
@@ -49,7 +64,11 @@ async def gather_data(page):
                       "91.0.4472.106 Safari/537.36"
     }
 
-    url = f"https://hh.ru/vacancies/analitik-dannyh?page={page}"
+    # url = f"https://hh.ru/vacancies/analitik-dannyh?page={page}"
+    url = (f"https://hh.ru/search/vacancy?area=1&search_field=name&search_field=company_name&search_field=description&"
+           f"enable_snippets=true&text=%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA+"
+           f"%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D1%85&only_with_salary=true&page={page}")
+
 
     async with aiohttp.ClientSession(trust_env=True) as session:
         response = await session.get(url=url, headers=headers)
@@ -58,13 +77,20 @@ async def gather_data(page):
         vacancy = soup.find_all(class_="vacancy-serp-item-body__main-info")
         vacancy_urls = []
         vacancy_titles = []
-        for el in vacancy[1:5]:
+        for el in vacancy:
             #get url of the current vacancy
             vacancy_url = el.find(class_="bloko-link").get("href")
             vacancy_urls.append(vacancy_url)
             #get title of the current vacancy
             vacancy_title = el.find(class_='serp-item__title').text
             vacancy_titles.append(vacancy_title)
+            # # get company name of the current vacancy
+            # vacancy_company_name = soup.find(class_="vacancy-serp-item__meta-info-company").text
+            #
+            #
+            #
+            # vacancy_experience = soup.find(class_="bloko-h-spacing-container bloko-h-spacing-container_base-0").text
+
 
         tasks = []
 
@@ -76,8 +102,22 @@ async def gather_data(page):
 
 
 def main():
-    page = 0
-    asyncio.run(gather_data(page))
+    for page in range(0, 39):
+        asyncio.run(gather_data(page))
+        current_time = time.time() - start_time
+        print(f"Information from page {page} was downloded. {round(current_time, 2)} seconds have passed.")
+
+
+    "create csv file with vacancy data"
+    columns = ['salary', 'title', 'url', 'company_name', 'experience']
+    with open('vacancy.csv', 'w', encoding='utf-8', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=columns, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+        writer.writeheader()
+        for row in job_vacancy_data_list:
+            writer.writerow(row)
+
+    finish_time = time.time() - start_time
+    print(f"Total time is {round(finish_time, 2)}")
 
 
 main()
